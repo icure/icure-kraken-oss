5ed06fb2dce739e4a93e827623c7b26b4a5070ce
LAPSAM6\sjocq <sam.jocque@medispring.be>
2023-01-30 11:41:03 +0100

    MS-8883: map changedToMale, changedToFemale and indeterminate to kmher CDSEXvalues acceptable values

--- src/main/java/org/taktik/icure/services/external/rest/v2/dto/be/ehealth/kmehr/v20180301/be/fgov/ehealth/standards/kmehr/cd/v1/CDSEXvalues.java
+++ src/main/java/org/taktik/icure/services/external/rest/v2/dto/be/ehealth/kmehr/v20180301/be/fgov/ehealth/standards/kmehr/cd/v1/CDSEXvalues.java
@@ -73,9 +73,21 @@ public enum CDSEXvalues {
         return value;
     }
 
+	static String mapGender(String v){
+		switch (v) {
+			case "changedToMale":
+			case "changedToFemale":
+				return "changed";
+			case "indeterminate":
+				return "undefined";
+			default:
+				return v;
+		}
+
+	}
     public static CDSEXvalues fromValue(String v) {
         for (CDSEXvalues c: CDSEXvalues.values()) {
-            if (c.value.equals(v)) {
+            if (c.value.equals(mapGender(v))) {
                 return c;
             }
         }
